0c9d5dcac27eec8c76b2520bd526336113aac25f
Hmmtrnt <rento5310@gmail.com>
2022-10-03 10:55:27 +0900

    弾の処理の移行

--- SceneMain.cpp
+++ SceneMain.cpp
@@ -11,7 +11,6 @@ SceneMain::SceneMain()
 {
 	m_hPlayerGraphic = -1;
 	m_hShotGraphic = -1;
-	m_shotInterval = 0;
 }
 SceneMain::~SceneMain()
 {
@@ -26,12 +25,12 @@ void SceneMain::init()
 
 	m_player.setHandle(m_hPlayerGraphic);
 	m_player.init();
+	m_player.setMain(this);
 
 	for (auto& shot : m_shot)
 	{
 		shot.setHandle(m_hShotGraphic);
 	}
-	m_shotInterval = 0;
 }
 
 // 終了処理
@@ -49,21 +48,6 @@ void SceneMain::update()
 	{
 		shot.update();
 	}
-	m_shotInterval--;
-	if (m_shotInterval < 0) m_shotInterval = 0;
-	// キー入力処理
-	int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
-	if ((padState & PAD_INPUT_1) && (m_shotInterval <= 0))
-	{
-		for (auto& shot : m_shot)
-		{
-			if (shot.isExist()) continue;
-
-			shot.start(m_player.getPos());
-			m_shotInterval = kShotInterval;
-			break;
-		}
-	}
 }
 
 // 毎フレームの描画
@@ -75,4 +59,24 @@ void SceneMain::draw()
 	{
 		shot.draw();
 	}
+
+	// 現在存在している弾の数を表示
+	int shotNum = 0;
+	for (auto& shot : m_shot)
+	{
+		if (shot.isExist()) shotNum++;
+	}
+	DrawFormatString(0, 0, GetColor(255, 255, 255), "弾の数：%d", shotNum);
+}
+
+bool SceneMain::createShot(Vec2 pos)
+{
+	for (auto& shot : m_shot)
+	{
+		if (shot.isExist()) continue;
+
+		shot.start(pos);
+		return true;
+	}
+	return false;
 }
